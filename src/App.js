3f00ae81d393c209d6b4e991aea213e4b7d0a7fe
Corwin Hiatt <corwinhiatt@yahoo.com>
2024-07-23 19:55:30 -0400

    I don't know where I'm Going with this yet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
 import './App.css';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <h1>Welcome to My Cool Webpage!</h1>
       </header>
+      <div className="text-boxes">
+        <input type="text" placeholder="Enter your name" />
+        <input type="text" placeholder="Enter your email" />
+        <input type="text" placeholder="Enter your message" />
+      </div>
     </div>
   );
 }
